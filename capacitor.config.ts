a058974e898b704f87f4fff9d65173b794320a69
Tarik IHADDADENE <tarik_li@hotmail.com>
2024-10-19 10:09:17 +0100

    initial project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.cinema',
+  appName: 'cinema',
+  webDir: 'www'
+};
+
+export default config;
